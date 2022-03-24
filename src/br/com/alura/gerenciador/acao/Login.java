f6985048c3a8bae2fc7dd7bb77091b4218d9aeb9
Wesley Vieira <wesleysv11@gmail.com>
2022-03-23 22:34:23 -0300

    primeiro commit

---
+++ src/br/com/alura/gerenciador/acao/Login.java
@@ -0,0 +1,43 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Usuario;
+
+public class Login implements Acao {
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		System.out.println("Logando " + login);
+		
+		//Verificando se o usuário existe
+		Banco banco = new Banco();
+		Usuario usuario = banco.existsUsuario(login, senha);
+		
+		if (usuario != null) {
+			System.out.println("Usuário existe no banco de dados de usuário");
+			
+			//Pergar o JSESSIONID, o cookie, para sessão do usuário...
+			HttpSession sessao = request.getSession();
+			sessao.setAttribute("usuarioLogado", usuario);
+			return "redirect:entrada?acao=ListaEmpresas";
+			
+		} else {
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
+		
+		
+	}
+
+}
